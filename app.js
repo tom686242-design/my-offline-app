97bdfc750eabbc83f3d832700843ef9c2972f326
tom686242-design <tom686242@gmail.com>
2026-05-03 18:39:29 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-    .then(() => console.log("Service Worker Registered"));
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js').then(reg => {
+            reg.onupdatefound = () => {
+                const installingWorker = reg.installing;
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                        // New version found! Reloading...
+                        window.location.reload();
+                    }
+                };
+            };
+        });
+    });
 }
